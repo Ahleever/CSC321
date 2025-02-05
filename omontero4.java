e512396012325ba83c450daec981ef269ea9e7ef
Ahleever <monterooliver97@gmail.com>
2025-02-05 02:09:28 +0000

    Updated code for lab4

--- omontero4.java
+++ omontero4.java
@@ -7,14 +7,30 @@ public class omontero4
 	{
 		int x = 0;
 		System.out.println("Choose an option: ");
-		System.out.println("Option 1: ");
-		System.out.println("Option 2: ");
-		System.out.println("Option 3: ");
-		System.out.println("Option 4: ");
+		System.out.println("Option 1");
+		System.out.println("Option 2");
+		System.out.println("Option 3");
+		System.out.println("Option 4");
 		
 		Scanner kb = new Scanner(System.in);
 		x = kb.nextInt();
 
-		System.out.println("You chose option " + x);	
+		System.out.println("You chose option " + x);
+		if (x == 1)
+		{
+			System.out.println("Went with the easy choice.");
+		} else if (x == 2)
+		{
+			System.out.println("Gotta use the restroom.");
+		}else if (x == 3)
+                {
+                        System.out.println("German vs British three.");
+                }else if (x == 4)
+                {
+                        System.out.println("We will evolve to have 4 fingers one day.");
+                } else
+		{
+			System.out.println("Nah, gotta choose a number listed.");
+		}
 	}
 }
