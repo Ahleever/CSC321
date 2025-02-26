826d8b69ef67138b1c25f173044d4fdfedad6f94
Ahleever <monterooliver97@gmail.com>
2025-02-26 02:02:29 +0000

    python files

---
+++ omontero7.py
@@ -0,0 +1,31 @@
+#Oliver Montero
+#Lab07
+
+x = 1
+y = 1.5
+z = "python"
+
+if x == 1:
+    print("You have 1 as a variable")
+else:
+    print("You have something else")
+if y == 1.5:
+    print("You have 1.5 as a variable")
+else:
+    print("You have something else")   
+if z == "python":
+    print("You have "'python'" as a variable")
+else:
+    print("You have something else")
+if x > 5:
+    print("X is greater than 5")
+else: 
+    print("X is not greater than 5")
+if y <= 2:
+    print("Y is less than 2")
+else: 
+    print("Y is greater than or equal to 2")
+if z == "California":
+    print("Hotel California")
+else:
+    print("We are playing with a snake")
